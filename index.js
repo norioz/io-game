9bac014ccd315f0c2a3a35bd34d1475240bdbef6
Brett Groshong <norioz@gmail.com>
2018-07-17 08:24:33 +0000

    Adds game screen and tutorial assets.

--- index.js
+++ index.js
@@ -3,22 +3,5 @@ const app = express()
 const port = 3000;
 
 app.use(express.static('public'))
-//app.get('/', (req, res) => res.send('Jello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-
-
-// const http = require('http');
-// const hostname = '127.0.0.1';
-// const port = 3000;
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hola, mundo\n');
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// 	       });
